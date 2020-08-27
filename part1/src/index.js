1159a8f2d2ae9b1d53f3c92ef0b2b7203c85a7e7
Kenji404 <kenji.andre@gmail.com>
2020-08-27 08:51:03 -0300

    0.2 - component inside component with props heritage

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,28 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = (props) => {
+
   return (
-    <h1>{props.course}</h1>
+    <p>{props.course}</p>
   );
 };
 
-const Part = (props) => {
+const Part = (props) =>  {
   return (
     <p>{props.part} {props.exercises}</p>
   );
 };
 
 const Content = (props) => {
+
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
 
 const Total = (props) => {
+
   return (
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   );
@@ -41,10 +44,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content  part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
-  )
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
