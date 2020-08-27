0646613f3e4b367ff431cf1c31de5a67c3157b2e
Kenji404 <kenji.andre@gmail.com>
2020-08-27 09:51:57 -0300

    working with props and objects inside an array

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Total of exercises {props.exercises0 + props.exercises1 + props.exercises2}</p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name0} {props.exercises0}</p>
+      <p>{props.name1} {props.exercises1}</p>
+      <p>{props.name2} {props.exercises2}</p>
+    </div>
+  );
+};
 
 const App = () => {
   const course = 'Half Stack application development';
@@ -8,24 +29,22 @@ const App = () => {
       name: 'Fundamentals of React',
       exercises: 10
     },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-];
-return (
-  <div>
-    <h1>{course}</h1>
-    <p>{parts[0].name} {parts[0].exercises}</p>
-    <p>{parts[1].name} {parts[1].exercises}</p>
-    <p>{parts[2].name} {parts[2].exercises}</p>
-    <p>Total of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  </div>
-);
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+  return (
+    <div>
+      <Header course={course} />
+      <Content name0={parts[0].name} name1={parts[1].name} name2={parts[2].name} exercises0={parts[0].exercises} exercises1={parts[1].exercises} exercises2={parts[2].exercises} />
+      <Total exercises0={parts[0].exercises} exercises1={parts[1].exercises} exercises2={parts[2].exercises} />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
